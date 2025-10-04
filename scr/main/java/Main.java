208a5bddefb33cadc3ebbb613946dd99920716b5
Enriko Matheus <enrikomatheus@gmail.com>
2025-10-04 18:33:48 -0300

    feat: adiciona divisao na Main

--- scr/main/java/Main.java
+++ scr/main/java/Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
+        Operacoes op = new Operacoes();
 
+        double resultado = op.divisao(1, 2);
+
+        System.out.println(resultado);
     }
 }
