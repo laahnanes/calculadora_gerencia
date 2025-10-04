a26785ec874d677a756673589b68c9694fbced6a
Ana Júlia <136942034+ailuj97@users.noreply.github.com>
2025-10-04 18:34:40 -0300

    feat: adiciona função de soma na Main

--- scr/main/java/Main.java
+++ scr/main/java/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Operacoes op = new Operacoes();
+
+        Scanner scanner = new Scanner(System.in);
+
+        double numero1 = scanner.nextDouble();
+        double numero2 = scanner.nextDouble();
+
+        double resultado = op.somar(numero1, numero2);
+
+        System.out.println(resultado);
 
     }
 }
