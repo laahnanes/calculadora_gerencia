0af9f94b2d55ddbe568620e064072183de73d131
laahnanes <larahnanes160@gmail.com>
2025-10-04 20:26:40 -0300

    fix: correção da divisão por 0

--- scr/main/java/Operacoes.java
+++ scr/main/java/Operacoes.java
@@ -9,7 +9,14 @@ public class Operacoes {
         return a - b;
     }
 
-    public double divisao (double a, double b) { return (a / b); }
+    public double divisao (double a, double b) {
+        if (b == 0) {
+            System.out.println("Erro: divisão por zero");
+            return 0;
+        }
+        return a / b;
+
+    }
 
     public double multiplicar (double a, double b) {
         if (a == 0 && b == 0) {
