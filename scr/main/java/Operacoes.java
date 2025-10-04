9214a266aba15aacced801709fad5624b1ab5e21
Enriko Matheus <enrikomatheus@gmail.com>
2025-10-04 17:52:49 -0300

    feat: adiciona recursao a multiplicacao

--- scr/main/java/Operacoes.java
+++ scr/main/java/Operacoes.java
@@ -6,6 +6,14 @@ public class Operacoes {
     }
 
     public double multiplicar (double a, double b) {
-        return a * b;
+        if (a == 0 && b == 0) {
+            return 0;
+        }
+
+        if (b == 1) {
+            return a;
+        }
+
+        return a * multiplicar(a, b - 1);
     }
 }
