1aa515b1093699a57b69c6046aee3e522baef61a
laahnanes <larahnanes160@gmail.com>
2025-10-05 15:15:00 -0300

    feat: conversão de centímetros para metros

--- scr/main/java/Conversoes.java
+++ scr/main/java/Conversoes.java
@@ -4,4 +4,8 @@ public class Conversoes {
         return metros * 100;
     }
 
+    public static double converterCentimetrosParaMetros(double centimetros) {
+        return centimetros / 100;
+    }
+
 }
