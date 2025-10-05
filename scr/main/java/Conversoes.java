a0495ea513ac033862366e3379cdeba7f6b67f4a
Enriko Matheus <enrikomatheus@gmail.com>
2025-10-05 16:59:36 -0300

    feat: implementa conversao metro e milimetro

--- scr/main/java/Conversoes.java
+++ scr/main/java/Conversoes.java
@@ -13,4 +13,10 @@ public class Conversoes {
     public static double converterQuilometrosParaMetros(double quilometros) {
         return quilometros * 1000;
     }
+    public static double converterMetrosParaMilimetros(double metros) {
+        return metros / 1000;
+    }
+    public static double converterMilimetrosParaMetros(double milimetros) {
+        return milimetros * 1000;
+    }
 }
